c2a4a95a32c3b6c04dc88e95738fc0da39ad4c10
GokhanYildiz <m.gokhan.yildiz@gmail.com>
2021-10-21 21:15:25 +0300

    add search view

--- app/src/main/java/com/example/mobilliumcase/ui/MainActivity.kt
+++ app/src/main/java/com/example/mobilliumcase/ui/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.mobilliumcase.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.mobilliumcase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
     }
 }
