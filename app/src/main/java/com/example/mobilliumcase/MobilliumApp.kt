573d160700067c3d2d4becd7df6ad93e3fbf90a1
GokhanYildiz <m.gokhan.yildiz@gmail.com>
2021-10-21 18:22:37 +0300

    add dependencies

---
+++ app/src/main/java/com/example/mobilliumcase/MobilliumApp.kt
@@ -0,0 +1,16 @@
+package com.example.mobilliumcase
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MobilliumApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (true) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
