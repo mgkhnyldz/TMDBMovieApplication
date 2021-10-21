301cba31d295c50b9badbb92c2d37cf7fcbda8a1
GokhanYildiz <m.gokhan.yildiz@gmail.com>
2021-10-21 19:15:00 +0300

    add fragment and nav host

--- app/src/main/java/com/example/mobilliumcase/ui/MainActivity.kt
+++ app/src/main/java/com/example/mobilliumcase/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-    package com.example.mobilliumcase
+package com.example.mobilliumcase.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobilliumcase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
